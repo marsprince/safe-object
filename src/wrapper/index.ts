834b8dcb317ceeff3d0d47d8b73e268906ff70df
liujia02 <bit_liujia@163.com>
2019-02-01 16:29:50 +0800

    add wrapErrors

--- src/wrapper/index.ts
+++ src/wrapper/index.ts
@@ -2,7 +2,7 @@ import { $Function } from '../@types/index';
 
 type Position = 'after' | 'before';
 export const wrapper = (originFn: $Function, wrapperFn: $Function, position: Position = 'after', argsWrapper?: $Function) => {
-  const temp = argsWrapper ? wrapperArgs(originFn, argsWrapper) : originFn;
+  const temp = argsWrapper ? wrapArgs(originFn, argsWrapper) : originFn;
   return function(...args) {
     if (position === 'before') {
       const wrapperResult = wrapperFn.apply(this, args);
@@ -18,9 +18,25 @@ export const wrapper = (originFn: $Function, wrapperFn: $Function, position: Pos
   };
 };
 
-export const wrapperArgs = (originFn: $Function, argsWrapper: $Function) => {
+export const wrapArgs = (originFn: $Function, argsWrapper: $Function) => {
   return function(...args) {
     const newArgs = argsWrapper.apply(this, args);
     return originFn.apply(this, newArgs);
   };
+};
+
+export const wrapErrors = (originFn: any, errorHandler: $Function, argsWrapper?: $Function) => {
+  const temp = argsWrapper ? wrapArgs(originFn, argsWrapper) : originFn;
+  if (!originFn.__wrapped__) {
+    originFn.__wrapped__ = function() {
+      try {
+        return temp.apply(this, arguments);
+      } catch (e) {
+        errorHandler(e); // report the error
+        throw e; // re-throw the error
+      }
+    };
+  }
+
+  return originFn.__wrapped__;
 };
