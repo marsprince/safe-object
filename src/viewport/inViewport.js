91f518fc6fb02c5dac6f4e13a39235346b398591
liujia02 <bit_liujia@163.com>
2019-04-18 10:56:12 +0800

    add inviewport

---
+++ src/viewport/inViewport.js
@@ -0,0 +1,9 @@
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Element/getBoundingClientRect
+export const inViewport = (el, partiallyVisible = false) => {
+  const { top, left, bottom, right } = el.getBoundingClientRect();
+  const { innerHeight, innerWidth } = window;
+  return partiallyVisible
+    ? ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) &&
+    ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+    : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+};
